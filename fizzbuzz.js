5886d0b9450eb2c8dbe01d451378be65af7a9741
st091249 <st091249@int.kcg.edu>
2017-02-27 12:17:05 +0900

    update to fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 var max = 20;
 var i, res;
 
-for (i = 0; i < max + 1; i++) {
+for (i = 1; i < max + 1; i++) {
   res =  (i % 3 == 0) ? 'Fizz' : '';
   res += (i % 5 == 0) ? 'Buzz' : '';
   res = (res != '') ? res : i;
