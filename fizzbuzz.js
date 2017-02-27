39fdb12d2c5378c012231d61cb8be33ccc0cd67d
st091249 <st091249@int.kcg.edu>
2017-02-27 11:56:18 +0900

    add to fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
-function FizzBuzz (max = 25) {
-  var i, res;
+var max = 20;
+var i, res;
 
-  for (i = 0; i < 25; i++) {
-    res =  (i % 3 == 0) ? 'Fizz' : '';
-    res += (i % 5 == 0) ? 'Buzz' : '';
-    console.log(i + ': ' + res);
-  }
+for (i = 0; i < max + 1; i++) {
+  res =  (i % 3 == 0) ? 'Fizz' : '';
+  res += (i % 5 == 0) ? 'Buzz' : '';
+  res = (res != '') ? res : i;
+  console.log(res);
 }
